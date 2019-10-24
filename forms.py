771a9d864a43a9aaebf4262a68ac63f4e4658551
Lucas Santos <lucasgbi146@gmail.com>
2019-10-24 09:49:43 -0300

    add arquivo forms.py

---
+++ forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+)
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+
+
+
+class CadastroForm(FlaskForm):
+    
+    username = StringField (
+        'Usuário',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    email = StringField (
+        'Email',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    password = PasswordField (
+        'Senha',
+        validators=[DataRequired()])
+    confirmar_password = StringField (
+        'Repita a Senha',
+        validators=[DataRequired(),EqualTo('password')])
