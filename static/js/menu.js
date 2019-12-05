f3876db7dc6bd0dc6810d971f1e4509dc83257cf
Tami Vitória <tamivitoria03@gmail.com>
2019-12-05 07:58:24 -0300

    ativar dropdown do menu

---
+++ static/js/menu.js
@@ -0,0 +1,9 @@
+$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false,
+    hover: true, 
+    gutter: 0, 
+    belowOrigin: false,
+    alignment: 'left'
+   });
