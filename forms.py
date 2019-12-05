d725cb8cd26e3edda19163f2f2584838e3445b8f
Tami Vitória <tamivitoria03@gmail.com>
2019-12-05 08:00:17 -0300

    add class CadastroEmpresaForm e LoginForm

--- forms.py
+++ forms.py
@@ -11,9 +11,22 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class CadastroUsuarioForm(FlaskForm):
     
+
+    nome_completo = StringField (
+        'Nome Completo',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    data_nascimento = StringField (
+        'Data de Nascimento',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    uf = StringField (
+        'Estado',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    cidade = StringField (
+        'Cidade',
+        validators=[DataRequired(), Length(min=2, max=80)])
     username = StringField (
         'Usuário',
-        validators=[DataRequired(), Length(min=2, max=80)])    
+        validators=[DataRequired(), Length(min=4, max=30)])    
     email = StringField (
         'Email',
         validators=[DataRequired(), Length(min=2, max=80)])
@@ -23,3 +36,40 @@ class CadastroUsuarioForm(FlaskForm):
     confirmar_password = StringField (
         'Repita a Senha',
         validators=[DataRequired(),EqualTo('password')])
+    
+
+
+class CadastroEmpresaForm(FlaskForm):
+    
+
+    nome_completo = StringField (
+        'Nome da Empresa',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    CNPJ = StringField (
+        'CNPJ',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    uf = StringField (
+        'Estado',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    cidade = StringField (
+        'Cidade',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    username = StringField (
+        'Usuário',
+        validators=[DataRequired(), Length(min=4, max=30)])    
+    email = StringField (
+        'Email',
+        validators=[DataRequired(), Length(min=2, max=80)])
+    password = PasswordField (
+        'Senha',
+        validators=[DataRequired()])
+    confirmar_password = StringField (
+        'Repita a Senha',
+        validators=[DataRequired(),EqualTo('password')])
+    
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Lembrar-me', validators=[DataRequired()])
