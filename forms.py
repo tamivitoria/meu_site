2678787ea62f34d04bbabf416f7b3ced11253c3d
Tami Vitória <tamivitoria03@gmail.com>
2019-12-09 08:33:51 -0300

    corrigindo as variaveis

--- forms.py
+++ forms.py
@@ -14,7 +14,7 @@ class CadastroUsuarioForm(FlaskForm):
     
     name = StringField ('Nome completo', validators=[DataRequired(), Length(min=2, max=80)])
     idade = IntegerField ('Idade', validators=[DataRequired()])
-    uf = StringField ('Estado', validators=[DataRequired(), Length(min=2, max=80)])
+    uf = StringField ('Estado', validators=[DataRequired(), Length(min=2, max=2)])
     cidade = StringField ('Cidade', validators=[DataRequired(), Length(min=2, max=80)])
     username = StringField ('Usuário', validators=[DataRequired(), Length(min=4, max=30)])    
     email = StringField ('Email', validators=[DataRequired(), Email(message="Email invalido"), Length(min=2, max=80)])
@@ -39,5 +39,5 @@ class CadastroEmpresaForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
     password = PasswordField('Senha', validators=[DataRequired()])
-    remember_me = BooleanField('Lembrar-me', validators=[DataRequired()])
+    remember_me = BooleanField('Lembre-me', validators=[DataRequired()])
     botao = SubmitField("Entrar")
